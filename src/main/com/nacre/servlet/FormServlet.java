3b46497b8566224f939ea8adb10afd37d81f4a4f
joshuag <joshuag@NYC1-JOSHUAG2.main.corp>
2010-08-10 17:06:12 -0400

    el kinda working

--- src/main/com/nacre/servlet/FormServlet.java
+++ src/main/com/nacre/servlet/FormServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tiles.Attribute;
+import org.apache.tiles.AttributeContext;
 import org.apache.tiles.TilesContainer;
 import org.apache.tiles.servlet.context.ServletUtil;
 import org.xml.sax.SAXException;
@@ -16,22 +18,31 @@ import com.nacre.service.vo.FormVO;
 
 public class FormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String XSD = "xsd";
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		response.getWriter().write("Hello");
-		String resourceName = "/test.xsd";
-		FormFactory ff;
+	private FormFactory formFactory = null;
+
+	@Override
+	public void init() throws ServletException {
 		try {
-			ff = new FormFactory(this.getClass().getResource(resourceName));
-			FormVO form = ff.getComplexType(request.getParameter("type"));
-			response.getWriter().write(form.toString());
+			formFactory = new FormFactory(this.getClass().getResource(this.getInitParameter(XSD)));
 		} catch (SAXException e) {
 			e.printStackTrace();
 			throw new ServletException(e);
 		}
+		super.init();
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().write("Hello");
+		FormVO form = formFactory.getComplexType(request.getParameter("type"));
+		response.getWriter().write(form.toString());
 		TilesContainer container = ServletUtil.getContainer(
 		        request.getSession().getServletContext());
+		AttributeContext ctx = container.startContext(request, response);
+		ctx.putAttribute("form", new Attribute(form));
 		container.render("nacre.doodad", request, response);
+		container.endContext(request, response);
 	}
 }
--- target/nacre/WEB-INF/classes/webapp/layouts/nacre.jsp
+++ target/nacre/WEB-INF/classes/webapp/layouts/nacre.jsp
@@ -1,5 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%><%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" isELIgnored="false" %>
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +9,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-<tiles:insertAttribute name="title" />
+<tiles:insertAttribute name="title" /><br />
+fields: ${fn:length(form.fields)}<br />
+<c:forEach items="${form.fields}" var="field" varStatus="fred">
+	${field.name}<br />
+</c:forEach>
+
 </body>
 </html>
